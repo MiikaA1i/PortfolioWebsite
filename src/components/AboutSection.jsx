49cc5bde1b30040a18639c2a5e6ffa676cd3ac1a
Malaika <malaika.a1ij@gmail.com>
2026-02-28 20:07:16 +1300

    Updating resume file.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/src/assets/Malaika Ali - Resume 2026.pdf" download="MalaikaAli-CV.pdf"
+                href="./MalaikaAli-Resume2026.pdf" download="MalaikaAli-Resume2026.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
